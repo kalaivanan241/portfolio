97a989ef89122020b2848414a526f20cc6a922a3
kalaivanan@squareark.com <kalaivanan@squareark.com>
2021-06-27 17:51:09 +0800

    fixed not found issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.scss";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Layout from "./components/Layout/Layout";
 import Experience from "./pages/Experience/Experience";
@@ -10,15 +15,16 @@ import NotFound from "./pages/NotFound/NotFound";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Layout>
+      <Layout>
+        <Switch>
           <Route component={Home} path="/" exact />
           <Route component={Experience} path="/experience" />
           <Route component={Skills} path="/skills" />
           <Route component={Contact} path="/contact-me" />
-          <Route component={NotFound} />
-        </Layout>
-      </Switch>
+          <Route path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </Layout>
     </Router>
   );
 }
